bb544581151a7324a8427e406b1d6d97cde04f68
Wesley Breno <74777926+Wesley-Breno@users.noreply.github.com>
2021-11-24 10:27:11 -0300

    Janela
    Criação de uma janela em branco, usando o PyQt5.

--- Interface/test.py
+++ Interface/test.py
@@ -1,24 +1,27 @@
-# Algoritmo que usei para poder criar uma janela em branco. 
+# Algoritmo usado para a criacao de uma janela em branco.
 
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys  # Para a gente poder interagir com a interface.
+from PyQt5.QtWidgets import QApplication, QMainWindow   # Fazer a criacao da janela.
 
-class Janela(QMainWindow):
+
+class Janela(QMainWindow):  # Criando o objeto 'janela'.
     def __init__(self):
-        super().__init__()
+        super().__init__()  # Coloquei a funcao super() para poder usar o QMainWindow sem nenhum problema.
 
+        # Aqui ficam os atributos da janela, largura, altura e etc.
         self.topo = 100
         self.esquerda = 100
         self.largura = 800
         self.altura = 600
-        self.titulo = 'Minha primeira janela'
-        self.CarregarJanela()
+        self.titulo = 'Janelona'
+        self.CarregarJanela()   
+        
+    def CarregarJanela(self):   # Funcao que vai fazer mostrar a janela com seus atributos e etc.
+        self.setGeometry(self.esquerda, self.topo, self.largura, self.altura)   # Colocar os atributos 
+        self.setWindowTitle(self.titulo)    # Colocar o titulo
+        self.show() # Mostrar a janela
 
-    def CarregarJanela(self):
-        self.setGeometry(self.esquerda, self.topo, self.largura, self.altura)
-        self.setWindowTitle(self.titulo)
-        self.show()
 
-aplicacao = QApplication(sys.argv)
-j = Janela()
-sys.exit(aplicacao.exec_())
+aplicacao = QApplication(sys.argv)  # Objeto que sera uma referencia do QApplication
+j = Janela()    #   Criando a janela
+sys.exit(aplicacao.exec_()) # Interaçao com a interface
