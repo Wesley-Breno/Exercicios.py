c0d7311599d2d1c098bdbcc9b590f9923b0b44fb
Wesley Breno <74777926+Wesley-Breno@users.noreply.github.com>
2022-06-05 05:13:21 -0300

    Aula 47
    Fazendo um codigo com menos linhas, e resolvendo o mesmo problema.

--- Udemy/aula47.py
+++ Udemy/aula47.py
@@ -1,14 +1,6 @@
 # Crie um laço de repetição onde do lado esquerdo ira contar
 # de 0 ate 9, e do lado esquerdo do 10 ate o 1.
 
-contador_crescente = 0
-contador_decrescente = 10
-
-for c in range(0, 3):   # Fazer pular 4 linhas
-    print()
-
 print('\033[;1m|Crescente|\t|Decrescente|\033[m')
-for c in range(0, 10):  # Fazendo a contagem dos dois lados.
-    print(f'\t{contador_crescente}\t\t\t{contador_decrescente}')
-    contador_crescente += 1
-    contador_decrescente -= 1
+for p, n in enumerate(range(10, 1, -1)):  # Fazendo a contagem dos dois lados. usando o enumerate para fazer o crescente e o range para o decrescente.
+    print(f'\t{p}\t\t\t{n}')
