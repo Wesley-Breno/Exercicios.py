fc1d324b0782bf2001e5e86035a7ec5e05fcbcd7
Wesley Breno <74777926+Wesley-Breno@users.noreply.github.com>
2022-06-09 13:37:11 -0300

    Exercício da 65 (refeito)
    Exercício da aula 65 refeito, por quê eu não tinha feito da forma que foi pedida na aula.

--- Udemy/aula65.py
+++ Udemy/aula65.py
@@ -1,66 +1,36 @@
-lista_numeros = [[1, 2, 3], [4, 5, 6, 6], [7, 8, 9], [9, 9, 9]]    # Lista que foi usada para o teste do algoritmo
+lista_de_listas_de_inteiros = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    [9, 1, 8, 9, 9, 7, 2, 1, 6, 8],
+    [1, 3, 2, 2, 8, 6, 5, 9, 6, 7],
+    [3, 8, 2, 8, 6, 7, 7, 3, 1, 9],
+    [4, 8, 8, 8, 5, 1, 10, 3, 1, 7],
+    [1, 3, 7, 2, 2, 1, 5, 1, 9, 9],
+    [10, 2, 2, 1, 3, 5, 10, 5, 10, 1],
+    [1, 6, 1, 5, 1, 1, 1, 4, 7, 3],
+    [1, 3, 7, 1, 10, 5, 9, 2, 5, 7],
+    [4, 7, 6, 5, 2, 9, 2, 1, 2, 1],
+    [5, 3, 1, 8, 5, 7, 1, 8, 8, 7],
+    [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+]
 
 
-def tem_duplicado(*args):
-    """
-    Função feita para verificar se tem algum
-    numero que apareça mais de uma vez (duplicado).
+def verificar_duplicado(lista):
+    numeros = set()
+    duplicado = -1  # Retornara -1 se nao houver nenhum numero duplicado.
 
-    :param args: parametro onde sera colocado a lista/tupla/set com seus numeros ou a sequencia de numeros.
-    :return: Retorna 0 se tiver numeros duplicados, retorna -1 se nao tiver numeros duplicados.
-    """
-    if len(args) > 1 and type(args) == tuple:   # Se o usuario tiver digitado numero por numero
-        lista = []  # Recriando a lista
-        numeros_duplicados = []     # Lista onde vai ficar os numeros duplicados
-        for n in args:
-            if n not in lista:  # Verificando se o numero nao esta na lista
-                lista.append(n)
-            else:
-                if n not in numeros_duplicados:     # Se o numero for duplicado, adiciona ele na lista de duplicados
-                    numeros_duplicados.append(n)
-        if len(numeros_duplicados) > 0:     # Se tiver numeros duplicados
-            # Retornando o 0 para dizer que tem numero multiplicado, retorna a lista, retorna os numeros duplicados
-            return 0, args, numeros_duplicados
-        else:
-            return -1, lista    # Retorna -1 se nao tiver numero duplicado, retorna a lista com os numeros analisados
+    for numero in lista:
+        if numero in numeros:  # Se achar um numero que ja foi adicionado ao conjunto 'numeros'
+            duplicado = numero
+            break
 
-    else:   # Se o usuario tiver colocado uma lista como parametro
-        numeros_duplicados = []
-        lista = []
-        # Verificando lista por lista para ver se tem numeros duplicados
-        for l1 in args:
-            for l2 in l1:
-                for n in l2:
-                    if n not in lista:
-                        lista.append(n)
-                    else:
-                        if n not in numeros_duplicados:
-                            numeros_duplicados.append(n)
-        if len(numeros_duplicados) > 0:
-            return 0, args, numeros_duplicados
-        else:
-            return -1, lista
+        numeros.add(numero)  # Se este numero nao foi adicionado ainda.
+
+    return duplicado
 
 
-res = tem_duplicado(lista_numeros)
-if len(res[1]) == 1:    # Se foi usado uma lista com numeros para fazer o analise
-    if res[0] == 0:     # Se tiver numero duplicado
-        print('\nTotal de numeros colocados.')
-        for l1 in res[1]:   # Escrevendo cada lista e seus numeros
-            for l2 in l1:
-                print(l2)
-        print(f'Numeros que foram duplicados: {res[2]}')
-    else:
-        print('\nTotal de numeros colocados.')
-        print(res[1])
-        print('Nenhum numero duplicado foi encontrado!')
-else:
-    if res[0] == 0:
-        print('\nTotal de numeros colocados.')
-        print(res[1])
-        print(f'Numeros que foram duplicados: {res[2]}')
-    else:
-        print('\nTotal de numeros colocados.')
-        print(res[1])
-        print('Nenhum numero duplicado foi encontrado!')
-
+for lista in lista_de_listas_de_inteiros:
+    print('__' * 18)
+    print(f'{lista}\n'
+          f'Primeiro numero duplicado -> {verificar_duplicado(lista)}')
+    print('__' * 18)
+    print()
