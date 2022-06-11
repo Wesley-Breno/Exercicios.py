276263ffb36ed7622c0d7ed825d89fee39d0903d
Wesley Breno <74777926+Wesley-Breno@users.noreply.github.com>
2022-06-11 19:13:26 -0300

    Exercício da aula 72 (refatorado)
    Exercício para mostrar os produtos comprados e seus preços, no final, mostrar o total a se pagar pela compra.

--- Udemy/aula72.py
+++ Udemy/aula72.py
@@ -1,21 +1,15 @@
-# Faca um algoritmo que mostre o valor total a se pagar por uma compra
-# usando list comprehension.
+lista_de_compras = [
+    {'produto 1': 25},
+    {'produto 2': 25},
+    {'produto 3': 50}
+]
 
-carrinho = [('Farofa yoki', 7), ('Vinho', 12), ('Regata', 109)]
+print('--' * 25)
 
-# Usando o list comprehension para pegar cada valor e soma-los
-# A variavel 'n' é o nome do produto, 'v' é o valor.
-# Transformando o valor em um numero flutuante e depois colocando tudo dentro de um sum() para finalmente somar tudo.
-total = sum([float(v) for n, v in carrinho])
-preco = (v for p, v in carrinho)    # Um gerador com o valor dos produtos.
-produtos = (p for p, v in carrinho)    # Um gerador com o nome dos produtos.
+valores_produtos = [valor for dicionario in lista_de_compras for chave, valor in dicionario.items()]  # Pegando cada preço
+valores_produtos = sum(valores_produtos)
 
-print(f'\n\n\033[;1m{"Produtos comprados":^45}\033[m\n')
-print('__' * 10)
-while True:    # Vai ficar escrevendo ate nao haver mais nenhum produto a ser mostrado e depois ira parar o loop.
-    try:
-        print(next(produtos), '> R\033[1;32m$\033[m', next(preco))  # Escrevendo o produto e em seguida o preco 1 por 1.
-    except:
-        break
-print('__' * 10)
-print(f'\nO total da sua compra foi R\033[1;32m$\033[m{total}\n')
+exibir = [print(k, f'-> R\033[;32m$\033[m{v:.2f}') for t in lista_de_compras for k, v in t.items()]  # Mostrando produtos e seus preçoes
+print(f'\ntotal a pagar: R\033[;32m$\033[m{valores_produtos:.2f}')
+
+print('--' * 25)
