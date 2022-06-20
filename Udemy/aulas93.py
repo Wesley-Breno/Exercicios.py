1c616f7ded328e290768a780724c29f86dc3ab1b
Wesley Breno <74777926+Wesley-Breno@users.noreply.github.com>
2022-06-20 01:27:00 -0300

    Exercício da aula 93 ( Refatorado )
    Faça uma lista de tarefas com as seguintes opções;
        * Adicionar tarefa
        * Listar tarefas
        * Desfazer ultima tarefa
        * Refazer ultima tarefa desfeita

--- Udemy/aulas93.py
+++ Udemy/aulas93.py
@@ -1,116 +1,76 @@
 """
-Faça uma lista de tarefas com as seguintes opções:
-    adicionar tarefa
-    listar tarefas
-    opção de desfazer (a cada vez que chamarmos, desfaz a última ação)
-    opção de refazer (a cada vez que chamarmos, refaz a última ação)
-    ['Tarefa 1', 'Tarefa 2']
-    ['Tarefa 1'] <- Desfazer
-    ['Tarefa 1', 'Tarefa 2'] <- Refazer
-    input <- Nova tarefa
+Faça uma lista de tarefas com as seguintes opções;
+    * Adicionar tarefa
+    * Listar tarefas
+    * Desfazer ultima tarefa
+    * Refazer ultima tarefa desfeita
 """
 
-from time import sleep    # Funcao usada para fazer o programa 'dormir'.
-from functions import titulo, pular, press_enter, erro, programa_encerrado    # Funcoes que criei.
+from time import sleep
 
-lista_tarefas = []    # Lista onde ficara todas as tarefas.
-lista_tarefas_desfeitas = []    # Lista onde ficara todas as tarefas que foram excluidas.
-
-titulo('Listas de tarefas')
-press_enter('para ir ao menu.')
+lista_tarefas = []
+tarefas_desfeitas = []
 
 while True:
-    pular(10)
+    print(f'\n\n\033[;1m{"Menu":^45}\033[m\n')
+    try:
+        acao = int(input('''
+Digite a acao que deseja fazer: 
 
-    print(f'\033[1;31m{"Menu":^45}\033[m\n')
-    try:    # Pedindo ao usuario para fazer uma escolha do que ele deseja fazer.
-        escolha = int(input('''
-| 1 | \033[1;31m➜\033[m Mostrar tarefas.
-| 2 | \033[1;31m➜\033[m Adicionar nova tarefa.
-| 3 | \033[1;31m➜\033[m Excluir ultima tarefa.
-| 4 | \033[1;31m➜\033[m Refazer tarefa excluida.
-| 5 | \033[1;31m➜\033[m Salvar tarefas.
+[ 1 ] - Adicionar tarefa
+[ 2 ] - Lista tarefas
+[ 3 ] - Desfazer ultima tarefa
+[ 4 ] - Refazer tarefa desfeita
 
-| 6 | \033[1;31m➜\033[m Informacoes.
-| 0 | \033[1;31m➜\033[m Encerrar programa.
+[ 0 ] - Encerrar programa
 
+Digite: '''))
 
-Escolha: '''))
-    except:    # Caso o usuario informe um dado invalido.
-        erro('Digite o numero da escolha\nque voce deseja fazer.')
-        pular(5)
+    except:
+        print('\n\n\n\033[1;31mERROR\033[m\nPor favor, tente novamente.\n\n\n')
+        sleep(2)
+
     else:
-        if escolha == 1:    # Mostrar tarefas.
-            pular(5)
-            print(f'\033[1;31m{"Tarefas":^45}\033[m\n\n')
+        print('__' * 25)
+        if acao == 1:
+            lista_tarefas.append(str(input('Digite a tarefa: ')).title().strip())
+            print(f'\n\n\033[;1m{"Tarefa adicionada com sucesso":^45}\033[m\n\n')
+            sleep(2)
 
-            if len(lista_tarefas) == 0:    # Caso o usuario nao adicionou nenhuma tarefa.
-                print(f'\033[;1m{"Voce ainda nao adicionou uma tarefa.":^45}\033[m\n\n')
+        elif acao == 2:
+            if len(lista_tarefas) > 0:
+                print(f'\n\n\033[;1m{"Todas as tarefas":^45}\033[m\n')
+                print(*lista_tarefas, sep='\n')
+                input('\n\nPressione enter para continuar.')
             else:
-                cont = 0    # Contador para colocar ao lado da tarefa, vai mostrar se é a primeira tarefa, segunda e etc.
-                for i in lista_tarefas:
-                    cont += 1
-                    print(f'{cont} \033[1;31m➤\033[m {i}')
-                print()
-            press_enter('para voltar ao menu.')
+                print(f'\n\n\033[;1m{"Nao existe nenhuma tarefa ainda":^45}\033[m\n\n')
+                sleep(2)
 
-        elif escolha == 2:    # Adicionar tarefas.
-            pular(5)
-            print(f'\033[1;31m{"Adicionar tarefa":^45}\033[m\n\n')
-
-            lista_tarefas.append(str(input('Nova tarefa ➤ ').strip().capitalize()))    # Adicionando nova tarefa.
-            print('Tarefa adicionada com \033[1;32msucesso\033[m.')
-            sleep(1)
-
-        elif escolha == 3:    # Excluir ultima tarefa.
-            if len(lista_tarefas) == 0:    # Se nao tiver nenhuma tarefa na lista.
-                print(f'\n\n\033[;1m{"Voce ainda nao adicionou uma tarefa.":^45}\033[m\n')
-                press_enter('para voltar ao menu.')
+        elif acao == 3:
+            if len(lista_tarefas) > 0:
+                tarefas_desfeitas.append(lista_tarefas[-1])
+                lista_tarefas.pop()
+                print(f'\n\n\033[;1m{"Ultima tarefa desfeita":^45}\033[m\n\n')
+                sleep(2)
             else:
-                lista_tarefas_desfeitas.append(lista_tarefas[-1])    # Adicionando para a lista de tarefas desfeitas.
-                lista_tarefas.pop()    # Apagando a ultima tarefa.
-                print('Tarefa excluida com \033[1;32msucesso\033[m.')
-                sleep(1)
+                print(f'\n\n\033[;1m{"Nao existe nenhuma tarefa ainda":^45}\033[m\n\n')
+                sleep(2)
 
-        elif escolha == 4:    # Refazer tarefa excluida.
-            if len(lista_tarefas_desfeitas) == 0:    # Se nao houver nenhuma tarefa na lista de tarefas excluidas.
-                print(f'\n\n\033[;1m{"Voce nao excluiu nenhuma tarefa ainda.":^45}\033[m\n')
-                press_enter('para voltar ao menu.')
+        elif acao == 4:
+            if len(tarefas_desfeitas) > 0:
+                lista_tarefas.append(tarefas_desfeitas[-1])
+                tarefas_desfeitas.pop()
+                print(f'\n\n\033[;1m{"Ultima tarefa refeita":^45}\033[m\n\n')
+                sleep(2)
             else:
-                lista_tarefas.append(lista_tarefas_desfeitas[-1])    # Adicionando a tarefa excluida de volta a lista de tarefas comum.
-                lista_tarefas_desfeitas.pop()    # Apagando a tarefa que estava 'apagada'.
-                print('Tarefa adicionada com \033[1;32msucesso\033[m.')
-                sleep(1)
+                print(f'\n\n\033[;1m{"Nao existe nenhuma tarefa desfeita ainda":^45}\033[m\n\n')
+                sleep(2)
 
-        elif escolha == 5:    # Informacoes do programa e exercicio proposto.
-            pular(5)
-            print(f'\033[1;31m{"Informacoes":^45}\033[m')
-            print('''
-Exercicio proposto na aula 93 do curso do Luiz Otavio, na plataforma Udemy.
-
-Exercicio;
-    
-    Faça uma lista de tarefas com as seguintes opções:
-    adicionar tarefa
-    listar tarefas
-    opção de desfazer (a cada vez que chamarmos, desfaz a última ação)
-    opção de refazer (a cada vez que chamarmos, refaz a última ação)
-    ['Tarefa 1', 'Tarefa 2']
-    ['Tarefa 1'] <- Desfazer
-    ['Tarefa 1', 'Tarefa 2'] <- Refazer
-    input <- Nova tarefa
-
-Exercicio feito por Wesley Breno.
-00:42 - 08/02/2022
-
-''')
-            press_enter('para voltar ao menu.')
-
-        elif escolha == 0:    # Encerrar programa.
+        elif acao == 0:
             break
 
-        else:    # Se o usuario digitar uma escolha diferente das que foram mostradas.
-            erro('Digite o numero da escolha\nque voce deseja fazer.')
-            pular(5)
+        else:
+            print('\n\n\n\033[1;31mERROR\033[m\nPor favor, tente novamente.\n\n\n')
+            sleep(2)
 
-programa_encerrado()    # Mensagem de despedida para o usuario.
+print(f'\n\n\033[;1m{"Programa encerrado com sucesso :)":^45}\033[m\n\n')
