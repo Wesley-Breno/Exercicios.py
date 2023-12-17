5ff190e013cd84261e7448313f4874665a5a6e10
Wesley Breno <74777926+Wesley-Breno@users.noreply.github.com>
2023-12-17 11:47:22 -0300

    Solution that exceeds 60% of users

---
+++ Exercicios de logica/leetcode/Exercicios faceis/Unique Morse Code Words/main.py
@@ -0,0 +1,40 @@
+codes = {
+    'a': ".-",
+    'b': "-...",
+    'c': "-.-.",
+    'd': "-..",
+    'e': ".",
+    'f': "..-.",
+    'g': "--.",
+    'h': "....",
+    'i': "..",
+    'j': ".---",
+    'k': "-.-",
+    'l': ".-..",
+    'm': "--",
+    'n': "-.",
+    'o': "---",
+    'p': ".--.",
+    'q': "--.-",
+    'r': ".-.",
+    's': "...",
+    't': "-",
+    'u': "..-",
+    'v': "...-",
+    'w': ".--",
+    'x': "-..-",
+    'y': "-.--",
+    'z': "--.."
+}
+
+
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        transformations = set()
+        for word in words:
+            transformation = ''
+            for letter in word:
+                transformation += codes[letter]
+            transformations.add(transformation)
+
+        return len(transformations)
