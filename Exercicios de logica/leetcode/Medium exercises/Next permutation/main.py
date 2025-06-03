356623babd5d12d392156d38cdcfa26d52fdb696
Wesley Breno <74777926+Wesley-Breno@users.noreply.github.com>
2025-06-03 15:32:48 -0300

    Add files via upload

---
+++ Exercicios de logica/leetcode/Medium exercises/Next permutation/main.py
@@ -0,0 +1,12 @@
+class Solution:
+    def nextPermutation(self, nums: list[int]) -> None:
+        n = len(nums)
+        i = n - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+        if i >= 0:
+            j = n - 1
+            while nums[j] <= nums[i]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+        nums[i + 1:] = reversed(nums[i + 1:])
